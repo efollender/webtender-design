231bc242059c2a6f2872cb1ac04c4fc4f6b822f6
Emma Follender <efollender@gmail.com>
2014-08-15 11:04:12 -0500

    Added route for adding recipes

--- server.rb
+++ server.rb
@@ -2,7 +2,7 @@
 require 'sinatra'
 require "sinatra/json"
 require 'rack-flash'
-require_relative 'lib/bartender_app.rb'
+#require_relative 'lib/bartender_app.rb'
 
 
 set :bind, '0.0.0.0' # Vagrant fix
@@ -22,11 +22,19 @@ get '/recipe/:id' do
 end
 
 get '/search/*id' do
-  @result = Bartender.dbi.get_search_results(params['id'])
+  #@result = Bartender.dbi.get_search_results(params['id'])
 end
 
 get '/add_recipe' do
-  puts params
-  @ingredients = Bartender.dbi.get_all_ingredients
+  #@ingredients = Bartender.dbi.get_all_ingredients
   erb :add_recipe
+end
+
+post '/add_recipe' do
+  count = (params.length - 3)/3
+  for i in 1..count+1
+    puts params['amount-'+i.to_s]
+
+  end
+  redirect to '/'
 end
