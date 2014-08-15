def2f1193283faf9e59b992b7043092d8da0f5b9
Emma Follender <efollender@gmail.com>
2014-08-15 12:18:34 -0500

    Added line counter to parser

--- parser.rb
+++ parser.rb
@@ -30,14 +30,21 @@ def getDrinks(num=1,limit=1000)
         ingrs = []
         ings.each do|x|
           if x.inner_html().include?("ingr")
-            # x.inner_text().split(" ")
-            x.inner_html().include?("ingr")
+            ing_arr = x.inner_text().split(" ")
+            if ing_arr[1]
+              if ing_arr[1][0].to_i > 0
+                ingrs << ing_arr[3..(ing_arr.length-1)].join(" ")
+              else
+                ingrs << ing_arr[2..(ing_arr.length-1)].join(" ")
+              end
+            end
           end
         end
-        puts Bartender::Recipe.new(drinkTitle, ingrs).name
+        method = doc.xpath("//td[@width='90%']/descendant::p").first.inner_text()
+        File.open('public/recipes.txt', 'a') { |file| file.puts({name: drinkTitle, ingredients: ingrs, directions: method }) } 
       end
     end
-    getdrinks(num+151)
+    getDrinks(num+151)
   else
     puts "done"
   end
@@ -58,7 +65,7 @@ def getIngredients(num=0,start=1,limit=1)
          doc = Nokogiri::HTML(open(link))
          drinkTitle = doc.xpath("//h1").inner_text()
          type = doc.xpath("//table[@width=220]/descendant::a").to_a().first.inner_text()
-         File.open('public/ingredients.txt', 'a') { |file| file.write({name: drinkTitle, type: type}) }
+         File.open('public/ingredients.txt', 'a') { |file| file.puts({name: drinkTitle, type: type}) }
       end
     end
     if num == 0
@@ -69,5 +76,7 @@ def getIngredients(num=0,start=1,limit=1)
     puts "done"
   end
 end
-getDrinks()
-
+getIngredients()
+count = 0
+File.open('public/ingredients.txt') {|f| count = f.read.count("\n")}
+puts count
